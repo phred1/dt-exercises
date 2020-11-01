05b9bc6f1200f3e7146547e19ae3a4751e7fe094
Philippe <philippe_reddy@icloud.com>
2020-11-01 01:55:25 -0400

    Pure Pursuite Follower

--- control/exercise_ws/src/lane_control/include/lane_controller/controller.py
+++ control/exercise_ws/src/lane_control/include/lane_controller/controller.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+class FollowPoint():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 class PurePursuitLaneController:
     """
@@ -11,8 +15,18 @@ class PurePursuitLaneController:
     """
 
     def __init__(self, parameters):
-
         self.parameters = parameters
+        self.distances_yellow = np.zeros(5)
+        self.distances_white = np.zeros(5)
+        self.distance = self.parameters['~follow_dist']
+        self.follow_point = FollowPoint(1,0)
+        self.is_pure = True
+    def from_segment(self, segment):
+        x = (segment.points[0].x + segment.points[1].x) / 2
+        y = (segment.points[0].y + segment.points[1].y) / 2
+        # print("x: " + str(x))
+        # print("y: " + str(y))
+        return  FollowPoint(x,y)
 
     def update_parameters(self, parameters):
         """Updates parameters of LaneController object.
@@ -21,5 +35,116 @@ class PurePursuitLaneController:
                 parameters (:obj:`dict`): dictionary containing the new parameters for LaneController object.
         """
         self.parameters = parameters
+        self.distances = []
+
+        self.previous_distances = []
+        self.previous_follow_points = []
+
+        self.set_follow_point_list(self.parameters["~yellow_lines"], self.parameters["~white_lines"])
+
+        self.set_distances()
+        self.set_follow_point()
+        
+    def middle(self, point1, point2):
+        x = (point1.x + point2.x) / 2
+        y = (point1.y + point2.y) / 2
+        middle = FollowPoint(x, y)
+        return middle
 
 
+    def set_follow_point_list(self, segments_yellow, segments_white):
+        possible_follow_points =  []
+        for segment in segments_yellow:
+            if segment.points[0].y >= 0:
+                possible_follow_points.append(self.from_segment(segment))
+        self.follow_points_yellow = possible_follow_points
+        # print("LEN_YELLOW")
+        # print(len(self.follow_points_yellow))
+        possible_follow_points =  []
+        for segment in segments_white:
+            if segment.points[0].y <= 0:
+                possible_follow_points.append(self.from_segment(segment))
+        self.follow_points_white= possible_follow_points
+
+        # print("LEN_WHITE")
+        # print(len(self.follow_points_white))
+
+
+    def set_distances(self):
+
+        distances =  []
+        for point in self.follow_points_yellow:
+            d = np.sqrt(point.x** 2 + point.y ** 2)
+            distances.append(d)
+        # print("D_YELLOW")
+        self.distances_yellow = np.asarray(distances)
+        # print(len(self.distances_yellow))
+        distances =  []
+        for point in self.follow_points_white:
+            d = np.sqrt(point.x** 2 + point.y ** 2)
+            distances.append(d)
+        # print("D_WHITE")
+        self.distances_white = np.asarray(distances)
+        # print(len(self.distances_white))
+    def get_nearest_to_ref(self, distances, reference_dist, is_current):
+        if not is_current:
+            reference_dist *= 1.5
+
+        idx = (np.abs(distances - reference_dist)).argmin()
+        return idx
+
+    def set_follow_point(self):
+        """
+            Input:
+                - follow_points: numpy array of possible follow points [x,y] in robot frame
+            Return:
+                - follow_point: numpty array of follow point closest to the ~follow_dist parameter
+        """
+        # self.previous_distance = self.distance
+        # self.previous_follow_point = self.follow_point
+
+        if self.distances_yellow.size > 0 and self.distances_white.size > 0 :
+            # print("CURRENT")
+            idx_yellow = self.get_nearest_to_ref(self.distances_yellow, self.parameters['~follow_dist'], True)
+            follow_point_yellow = self.follow_points_yellow[idx_yellow]
+            # print("FOLLOW_POINT_YELLOW")
+            # print(follow_point_yellow.x)
+            idx_white = self.get_nearest_to_ref(self.distances_white, self.parameters['~follow_dist'], True)
+            follow_point_white = self.follow_points_white[idx_white]
+            # print("FOLLOW_POINT_WHITE")
+            # print(follow_point_white.x)
+            self.follow_point = self.middle(follow_point_yellow, follow_point_white)
+            self.distance = np.sqrt(self.follow_point.x** 2 + self.follow_point.y ** 2)
+        else:
+            self.is_pure = False
+        #     print("PREVIOUS")
+        #     self.distance = self.previous_distance
+        #     self.follow_point = self.previous_follow_point
+
+
+    def pure_pursuit(self):
+        """
+        Input:
+            - follow_points: numpy array of follow points [x,y] in robot frame
+            - K: controller gain
+        Return:
+            - v: linear velocity in m/s (float)
+            - w: angular velocity in rad/s (float)
+        """
+
+        # compute distance between robot and follow point
+        # print("DISTANCE")
+        # print(self.distance)
+        # TODO: compute sin(alpha)
+        sin_alpha =  self.follow_point.y / self.distance
+        
+        v = 0.1 # we can make it constant or we can make it as a function of sin_alpha
+        
+        # TODO: compute angular velocity
+        w = None
+        if self.is_pure:
+            w = sin_alpha / self.parameters["~K"]
+
+        self.is_pure = True
+
+        return v, w
--- control/exercise_ws/src/lane_control/src/lane_controller_node.py
+++ control/exercise_ws/src/lane_control/src/lane_controller_node.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
 import rospy
-
+import inspect
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
-from duckietown_msgs.msg import Twist2DStamped, LanePose, WheelsCmdStamped, BoolStamped, FSMState, StopLineReading
+from duckietown_msgs.msg import SegmentList, Twist2DStamped, LanePose, WheelsCmdStamped, BoolStamped, FSMState, StopLineReading
 
 from lane_controller.controller import PurePursuitLaneController
 
@@ -20,6 +20,8 @@ class LaneControllerNode(DTROS):
         ~car_cmd (:obj:`Twist2DStamped`): The computed control action
     Subscribers:
         ~lane_pose (:obj:`LanePose`): The lane pose estimate from the lane filter
+        ~seglist_filtered (:obj:``SegmentList): Filtered list of segments that are considered as valid
+        ~lineseglist_out (:obj:`duckietown_msgs.msg.SegmentList`): Line segments in the ground plane relative to the robot origin
     """
 
     def __init__(self, node_name):
@@ -32,6 +34,9 @@ class LaneControllerNode(DTROS):
 
         # Add the node parameters to the parameters dictionary
         self.params = dict()
+        self.params['~follow_dist'] = rospy.get_param('~follow_dist', None)
+        self.params['~K'] = rospy.get_param('~K', None)
+
         self.pp_controller = PurePursuitLaneController(self.params)
 
         # Construct publishers
@@ -45,24 +50,89 @@ class LaneControllerNode(DTROS):
                                                  LanePose,
                                                  self.cbLanePoses,
                                                  queue_size=1)
-
+        self.log("INIT SEGLIST FILTERED")
+        self.sub_seglist_filtered = rospy.Subscriber("/agent/lane_filter_node/seglist_filtered",
+                                                 SegmentList,
+                                                 self.cbSeglistFiltered,
+                                                 queue_size=1)
+        self.log("INIT SEGLIST OUT")                                     
+        self.sub_seglist_filtered = rospy.Subscriber("/agent/ground_projection_node/lineseglist_out",
+                                                 SegmentList,
+                                                 self.cbSeglistOut,
+                                                 queue_size=1)
         self.log("Initialized!")
+
+
+    def cbSeglistFiltered(self, input_seglist_filtered):
+        """Callback receiving pose messages
+
+        Args:
+            input_seglist_filtered (:obj:`SegmentList`): Message containing information about filtered list of segments that are considered as valid.
+        """
+        yellow_lines = []
+        white_lines = []
+        for line in input_seglist_filtered.segments:
+            if line.color == 1:
+                yellow_lines.append(line)
+            elif line.color == 0:
+                white_lines.append(line) 
+        self.params["~yellow_lines: "] = yellow_lines
+        self.params["~white_lines"] = white_lines
+        self.cbParametersChanged()
+    
+
+    def cbSeglistOut(self, input_seglist_out):
+        """Callback receiving pose messages
+
+        Args:
+            input_seglist_out(:obj:`SegmentList`): Message containing information line segments in the ground plane relative to the robot origin.
+        """
+        # self.log("SEGLIST_OUT")
+        yellow_lines = []
+        white_lines = []
+        for line in input_seglist_out.segments:
+            if line.color == 1:
+                yellow_lines.append(line)
+            elif line.color == 0:
+                white_lines.append(line) 
+        # self.log("ground_yellow: " + str(len(yellow_lines)))
+        self.params["~yellow_lines"] = yellow_lines
+        self.params["~white_lines"] = white_lines
+        self.cbParametersChanged()
+        # self.log(len(white_lines))
+    
 
     def cbLanePoses(self, input_pose_msg):
         """Callback receiving pose messages
 
         Args:
-            input_pose_msg (:obj:`LanePose`): Message containing information about the current lane pose.
+            input_pose_msg (:obj:`LanePose`): Line segments in the ground plane relative to the robot origin.
         """
+        # self.log("POSE")
+        # self.log(input_pose_msg.phi)
         self.pose_msg = input_pose_msg
-
         car_control_msg = Twist2DStamped()
         car_control_msg.header = self.pose_msg.header
 
-        # TODO This needs to get changed
-        car_control_msg.v = 0.5
-        car_control_msg.omega = 0
-
+        # # TODO This needs to get changed
+        v, omega = self.pp_controller.pure_pursuit()
+        # print("OMEGA")
+        # print(omega)
+        car_control_msg.v =  v
+        car_control_msg.omega = omega
+        if omega :
+            print("PURE")
+            car_control_msg.omega = omega
+        else:
+            print("NOT PURE")
+            print(self.pose_msg.phi)
+            o = np.sin(self.pose_msg.phi + np.pi) + self.pose_msg.d
+            car_control_msg.omega = o
+            print("omega_computed: " + str(o))
+        print("phi: " + str(self.pose_msg.phi))
+        print("d: " + str(self.pose_msg.d))
+        print("in_lane: " + str(self.pose_msg.in_lane))
+    
         self.publishCmd(car_control_msg)
 
 
@@ -78,7 +148,7 @@ class LaneControllerNode(DTROS):
     def cbParametersChanged(self):
         """Updates parameters in the controller object."""
 
-        self.controller.update_parameters(self.params)
+        self.pp_controller.update_parameters(self.params)
 
 
 if __name__ == "__main__":
